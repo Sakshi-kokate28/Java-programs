cff4d11de7749e918ca7fbb4636d1b6aa65bb90e
Sakshi Kokate <kokatesakshi0@gmail.com>
2026-09-10 18:46:38 +0530

    Add files via upload

---
+++ Encapsulation.java
@@ -0,0 +1,40 @@
+class Marvellous
+{
+    public int No1;
+    public int No2;
+
+    public void fun()
+    {
+        System.out.println("Inside Fun");
+    }
+
+}
+class Encapsulation
+{
+    public static void main(String A[]){
+
+    Marvellous mobj = new Marvellous();
+    mobj.fun();
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
