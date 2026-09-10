37d3c700fda99e89c55ba328ba35d02bcaab049c
Sakshi Kokate <kokatesakshi0@gmail.com>
2026-09-10 19:13:13 +0530

    Add files via upload

---
+++ Addition.java
@@ -0,0 +1,31 @@
+import java . util.*;
+class Addition
+{
+    public static void main(String A[])
+  {
+    int a  = 0, b = 0 , Sum = 0;
+
+    Scanner sobj = new Scanner(System.in);
+
+    System.out.println("Enter a First Number");
+    a = sobj.nextInt();
+
+    System.out.println("Enter a Second  Number");
+    b = sobj.nextInt();
+
+    Sum = a + b;
+
+    System.out.println("Addition of the Number is" +Sum);
+
+
+
+
+
+
+
+
+    
+    }
+}
+
+
