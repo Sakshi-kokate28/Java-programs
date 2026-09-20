ad0606439b546fb862e2910755ce99f3e108e6b8
Sakshi Kokate <kokatesakshi0@gmail.com>
2026-09-20 09:18:14 +0530

    Add files via upload

---
+++ Arraymodifier.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int arr[5]={11,21,51,101,111};
+
+    cout<<arr[0]<< " " <<arr[1]<< " " <<arr[2]<<
+    
+    arr[2] = 60;
+
+    cout<<arr[0]<< " " <<arr[1]<< " " <<arr[2]<< " " <<arr[3]<< "  " <<arr[4]<<
+
+  return 0;
+    
+
+
+
+    
+
+}
+
