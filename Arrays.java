f3a1fb13d35f29397dee60ef200498098c19136d
Sakshi Kokate <kokatesakshi0@gmail.com>
2026-09-20 08:56:17 +0530

    Add files via upload

---
+++ Arrays.java
@@ -0,0 +1,12 @@
+class Arrays
+{
+    public static void main(String A[])
+    {
+        int[] arr = {3,6,7} ;
+        System.out.println(arr[0]);
+        System.out.println(arr[1]);
+        System.out.println(arr[2]);
+
+        
+    }
+}
